5d106a1cd27efd52f00f9bfea3d1f3d3df513aee
Hamza Ben Attia <146210070+hamzabenattia@users.noreply.github.com>
2024-01-28 15:54:06 +0100

    Update SingleAds.js

--- frontend/src/Pages/SingleAds.js
+++ frontend/src/Pages/SingleAds.js
@@ -25,9 +25,9 @@ const SingleAds = () => {
       <Header />
 
       {isLoading ?
-        <div className="flex justify-center items-center">
-          <div className="animate-spin rounded-full h-32 w-32 border-t-2 border-b-2 border-gray-900"></div>
-        </div>
+       <div className="text-center">
+          <Spinner/>
+      </div> 
         :
         <section className="py-10 font-poppins dark:bg-gray-800">
           <div className="max-w-6xl px-4 mx-auto">
